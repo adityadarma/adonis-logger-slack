73bfedfef8007680a5258fb7f59cb269f254909f
Aditya Darma <adhit.boys1@gmail.com>
2024-11-06 11:09:52 +0800

    chore: add release-it package

--- src/slack.ts
+++ src/slack.ts
@@ -3,12 +3,12 @@ import * as slack from '@slack/webhook'
 
 export class Slack extends slack.IncomingWebhook {
   sendException(error: any) {
-    const slack = config.get<any>('logger.loggers.slack')
+    const configSlack = config.get<any>('logger.loggers.slack')
 
     this.send({
       attachments: [
         {
-          color: this.setColor(slack.level),
+          color: this.setColor(configSlack.level),
           blocks: [this.setHeader(error.message), this.setContent(error.stack)],
         },
       ],

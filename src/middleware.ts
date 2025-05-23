52392c303c05a4ddeff825ea890df39376cc9087
Aditya Darma <adhit.boys1@gmail.com>
2025-05-23 17:04:54 +0800

    fix: handle when default not slack

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,11 +2,15 @@ import { HttpContext } from '@adonisjs/core/http'
 import type { NextFn } from '@adonisjs/core/types/http'
 import { slack } from '../services/main.js'
 import { Slack } from './slack.js'
+import app from '@adonisjs/core/services/app'
 
 export default class SlackMiddleware {
   async handle(ctx: HttpContext, next: NextFn) {
-    ctx.slack = slack
-    ctx.containerResolver.bindValue(Slack, ctx.slack)
+    const configApp = app.config.get<any>('logger.loggers.default')
+    if (configApp === 'slack') {
+      ctx.slack = slack
+      ctx.containerResolver.bindValue(Slack, ctx.slack)
+    }
 
     return await next()
   }
